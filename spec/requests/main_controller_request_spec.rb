85099b0d9eb57047823aba4a23d3a91adb982995
PoisonousShadow <poisonousshadow@gmail.com>
2020-12-02 14:01:58 +0300

    without Capybara

--- spec/requests/main_controller_request_spec.rb
+++ spec/requests/main_controller_request_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe "MainControllers", type: :request do
     end
 
     context MainController, type: :controller do
-        context 'Anonim' do
+        context 'User' do
             login_user
         it{expect(get :index).to have_http_status(200)}
         end
     end
 
     context MainController, type: :controller do
-        context 'Anonim' do
+        context 'Admin' do
             login_admin
         it{expect(get :index).to have_http_status(200)}
         end
